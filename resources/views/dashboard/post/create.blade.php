61a0e3debf761cad1e4333ea7632102791f28513
alex <a.morcillo@smdos.es>
2020-10-05 11:55:05 +0200

    Update create.blade.php

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -1,4 +1,14 @@
 <form action="" method="POST">
-    <input type="text" name="" >
-
+    <div class="form-group">
+        <label for="title">Título</label>
+        <input  class= "form-control" type="text" name="title" id="title" >
+    </div>
+    <div class="form-group">
+        <label for="url_clean">Url limpia</label>
+        <input  class= "form-control" type="text" name="url_clean" is="url_clean">
+    </div>
+    <div class="form-group">
+        <label for="content">Contenido</label>
+        <textarea class= "form-control" name="content" id="content"  rows="3"></textarea>
+    </div>
 </form>
